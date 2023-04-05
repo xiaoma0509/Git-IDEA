de323a3c3a99353861d88563f54ee90df588ce95
Cy <xiaoma0509@vip.qq.com>
2023-04-05 19:13:44 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * Date:2023/4/5
+ * author: C.y
+ * description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
