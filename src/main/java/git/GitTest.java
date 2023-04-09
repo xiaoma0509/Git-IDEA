76c7130daada1b60410f975daabb5bfe4eeed7a5
Cy <xiaoma0509@vip.qq.com>
2023-04-10 03:50:47 +0800

    hot-fix commit2

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git22222!");
         System.out.println("hot-fix");
+        System.out.println("hot-fix test");
     }
 }
